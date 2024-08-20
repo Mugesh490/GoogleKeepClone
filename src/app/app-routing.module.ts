aa615a86ff7da31cbd8ce693121e34f6efa81d13
Mugesh490 <mrmugeshm@gmail.com>
2024-08-20 16:17:24 +0530

    added sidebar navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,54 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+// import { BirthdayDetailsComponent } from './birthday-details/birthday-details.component';
+import { RemainderComponent } from './components/remainder/remainder.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NotesContainerComponent } from './components/notes-container/notes-container.component';
+import { ArchiveComponent } from './components/archive/archive.component';
+import { TrashComponent } from './components/trash/trash.component';
+import { AuthGuard } from './service/auth/auth.guard';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+    { 
+        path: 'register',
+        component: RegisterComponent,
+
+    },
+    {path:'login', component:LoginComponent},
+    //{path:'birthdayDetails',component:BirthdayDetailsComponent},
+    {
+        path:'',
+        component:DashboardComponent,
+        canActivate:[AuthGuard],
+        children:[
+            {
+                path:'notes',
+                component:NotesContainerComponent
+            },
+            {
+                path:'archive',
+                component:ArchiveComponent
+            },
+            {
+                path:'trash',
+                component:TrashComponent
+            },
+            {
+                path:'remainder',
+                component:RemainderComponent
+            }
+        ]
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [
+        RouterModule
+    ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,64 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClient,HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AddnoteComponent } from './components/addnote/addnote.component';
+import { ArchiveComponent } from './components/archive/archive.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotecardComponent } from './components/notecard/notecard.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TrashComponent } from './components/trash/trash.component';
+import { NotesContainerComponent } from './components/notes-container/notes-container.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggle, MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatIconModule} from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FormsModule } from '@angular/forms';
+import { RemainderComponent } from './components/remainder/remainder.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    AddnoteComponent,
+    ArchiveComponent,
+    NavbarComponent,
+    NotecardComponent,
+    DashboardComponent,
+    SidebarComponent,
+    TrashComponent,
+    NotesContainerComponent,
+    RemainderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
